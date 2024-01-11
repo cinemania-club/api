9e084f86ddd2105c9cb87f7df8199d5168b55e03
Guto <gutomarzagao@gmail.com>
2024-01-10 23:13:18 -0300

    .

--- src/worker/scrapper/scrapper.module.ts
+++ src/worker/scrapper/scrapper.module.ts
@@ -1,6 +1,5 @@
 import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
 import { MovieRepository } from './movie.repository';
 import { ScrapperService } from './scrapper.service';
 import { TmdbAdapter } from './tmdb.adapter';
@@ -10,7 +9,6 @@ import { TmdbProcessor } from './tmdb.processor';
   imports: [
     BullModule.forRoot({ redis: { host: 'redis', port: 6379 } }),
     BullModule.registerQueue({ name: 'tmdb' }),
-    MongooseModule.forRoot('mongodb://mongo/letsMovie'),
   ],
   providers: [ScrapperService, TmdbProcessor, MovieRepository, TmdbAdapter],
 })
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,8 +1,4 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
-
+/*
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
@@ -22,3 +18,4 @@ describe('AppController (e2e)', () => {
       .expect('Hello World!');
   });
 });
+*/
