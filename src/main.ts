fec15ca576414c32e1477775ef1de9d71ffc2359
Guto Marzagão <gutomarzagao@gmail.com>
2024-01-14 00:46:24 -0300

    Track exceptions with Sentry (#5)

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
-import { NestFactory } from "@nestjs/core";
+import { HttpAdapterHost, NestFactory } from "@nestjs/core";
+import * as Sentry from "@sentry/node";
 import { AppModule } from "./app.module";
+import { SentryFilter } from "./sentry.filter";
 import { WorkerModule } from "./worker/worker.module";
 
 async function bootstrap() {
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    environment: process.env.NODE_ENV,
+  });
+
   const module = process.env.WORKER ? WorkerModule : AppModule;
   const app = await NestFactory.create(module);
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new SentryFilter(httpAdapter));
+
   await app.listen(3000);
 }
 bootstrap();
