50b1737a56c36f35d3e489577ea9255d232dd564
Guto <gutomarzagao@gmail.com>
2024-01-14 01:20:40 -0300

    Track exceptions on worker module using Sentry

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,9 @@ import { WorkerModule } from "./worker/worker.module";
 
 async function bootstrap() {
   Sentry.init({
-    dsn: process.env.SENTRY_DSN,
+    dsn: process.env.WORKER
+      ? process.env.SENTRY_DSN_WORKER
+      : process.env.SENTRY_DSN_API,
     environment: process.env.NODE_ENV,
   });
 
--- src/worker/processor.ts
+++ src/worker/processor.ts
@@ -1,16 +1,22 @@
 import { OnQueueFailed } from "@nestjs/bull";
+import * as Sentry from "@sentry/node";
 import { Job } from "bull";
 
 export abstract class BaseProcessor {
   @OnQueueFailed()
   jobFailed(job: Job, error: Error) {
-    const details = JSON.stringify({
+    const details = {
       name: job.name,
       id: job.id,
       data: job.data,
-    });
+    };
 
     const msg = error.stack || error.message;
-    console.error(`[Worker] Job failed. Job details: ${details}. ${msg}`);
+    console.error(
+      `[Worker] Job failed. Job details: ${JSON.stringify(details)}. ${msg}`,
+    );
+    Sentry.captureException(error, {
+      contexts: { job: details },
+    });
   }
 }
