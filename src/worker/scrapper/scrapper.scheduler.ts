00e10d7cf537e05e05ff97bdf391fcffc2bfa6f5
Guto <gutomarzagao@gmail.com>
2024-01-19 00:04:23 -0300

    .

--- src/worker/scrapper/scrapper.scheduler.ts
+++ src/worker/scrapper/scrapper.scheduler.ts
@@ -3,12 +3,13 @@ import { Injectable } from "@nestjs/common";
 import { Cron } from "@nestjs/schedule";
 import { Queue } from "bull";
 import { startOfDay, subDays } from "date-fns";
+import { BRT, isProduction } from "src/constants";
 
 @Injectable()
 export class ScrapperScheduler {
   constructor(@InjectQueue("tmdb") private tmdbQueue: Queue) {}
 
-  @Cron("0 0 15 * * *", { disabled: process.env.NODE_ENV !== "production" })
+  @Cron("0 0 12 * * *", { timeZone: BRT, disabled: !isProduction })
   async getChanges() {
     const today = startOfDay(new Date());
     const yesterday = subDays(today, 1);
--- src/worker/worker.module.ts
+++ src/worker/worker.module.ts
@@ -2,11 +2,17 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
 import { ScheduleModule } from "@nestjs/schedule";
+import * as Joi from "joi";
 import { ScrapperModule } from "./scrapper/scrapper.module";
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid("production", "development", "test"),
+      }),
+    }),
     ScheduleModule.forRoot(),
     MongooseModule.forRoot("mongodb://mongo/cinemania"),
     ScrapperModule,
