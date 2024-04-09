635dca2690fd04be63e4551197470e9703787a9d
Bruno Franco <102704410+brunomarfranco@users.noreply.github.com>
2024-04-09 20:00:10 -0300

    Filter movie by search criterias (#8)

---
+++ src/movie/dto/movieFilter.dto.ts
@@ -0,0 +1,39 @@
+import {
+  IsArray,
+  IsDateString,
+  IsEnum,
+  IsInt,
+  Max,
+  Min,
+} from "class-validator";
+
+export enum OrderBy {
+  CREATED_AT_ASC = "CREATED_AT_ASC",
+  CREATED_AT_DESC = "CREATED_AT_DESC",
+  RELEASE_DATE_ASC = "RELEASE_DATE_ASC",
+  RELEASE_DATE_DESC = "RELEASE_DATE_DESC",
+}
+
+export class MovieFilterDto {
+  @IsInt()
+  @Min(0)
+  @Max(600)
+  minRuntime: number;
+  @IsInt()
+  @Min(0)
+  @Max(600)
+  maxRuntime: number;
+
+  @IsDateString()
+  minReleaseDate: string;
+  @IsDateString()
+  maxReleaseDate: string;
+
+  @IsArray()
+  genres: number[];
+  @IsArray()
+  requiredGenres: number[];
+
+  @IsEnum(OrderBy)
+  orderBy: OrderBy;
+}
--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,14 +1,54 @@
-import { Controller, Get } from "@nestjs/common";
+import { Body, Controller, Get } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Movie } from "src/movie/movie.schema";
+import { MovieFilterDto, OrderBy } from "./dto/movieFilter.dto";
+
+const SORT_QUERY = {
+  [OrderBy.CREATED_AT_ASC]: { createdAt: 1 },
+  [OrderBy.CREATED_AT_DESC]: { createdAt: -1 },
+  [OrderBy.RELEASE_DATE_ASC]: { release_date: 1 },
+  [OrderBy.RELEASE_DATE_DESC]: { release_date: -1 },
+};
 
 @Controller("/movies")
 export class MovieController {
   constructor(@InjectModel(Movie.name) private movieModel: Model<Movie>) {}
 
   @Get()
-  async getMovies() {
-    return this.movieModel.find();
+  async getMovies(@Body() movieFilter: MovieFilterDto) {
+    const minReleaseDate = new Date(movieFilter.minReleaseDate);
+    const maxReleaseDate = new Date(movieFilter.maxReleaseDate);
+
+    const genres = movieFilter.genres.map((e) => ({
+      genres: { $elemMatch: { id: e } },
+    }));
+
+    const requiredGenres = movieFilter.requiredGenres.map((e) => ({
+      genres: { $elemMatch: { id: e } },
+    }));
+
+    return this.movieModel.find(
+      {
+        $and: [
+          {
+            runtime: {
+              $gte: movieFilter.minRuntime,
+              $lte: movieFilter.maxRuntime,
+            },
+          },
+          {
+            release_date: {
+              $gte: minReleaseDate,
+              $lte: maxReleaseDate,
+            },
+          },
+          ...requiredGenres,
+          { $or: genres.length ? genres : [{}] },
+        ],
+      },
+      {},
+      { sort: SORT_QUERY[movieFilter.orderBy] },
+    );
   }
 }
