9818662d87c77c7bdb2e2053ebf62dbdfdbd192e
bruno <brunomarfranco@gmail.com>
2024-04-16 21:02:50 -0300

    .

--- src/movie/movie.service.ts
+++ src/movie/movie.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
-import { Model } from "mongoose";
+import { Model, Types } from "mongoose";
 import { Movie } from "src/movie/movie.schema";
 import { MovieFiltersDto, OrderBy } from "./dto/movie-filters.dto";
+import { MovieVote } from "./movie-vote.schema";
 
 const SORT_QUERY = {
   [OrderBy.CREATED_AT_ASC]: { createdAt: 1 },
@@ -13,21 +14,24 @@ const SORT_QUERY = {
 
 @Injectable()
 export class MovieService {
-  constructor(@InjectModel(Movie.name) private movieModel: Model<Movie>) {}
+  constructor(
+    @InjectModel(Movie.name) private movieModel: Model<Movie>,
+    @InjectModel(MovieVote.name) private movieVoteModel: Model<MovieVote>,
+  ) {}
 
-  async getMovies(filters: MovieFiltersDto) {
+  async getMovies(filters: MovieFiltersDto, userId: Types.ObjectId) {
     const minReleaseDate = new Date(filters.minReleaseDate);
     const maxReleaseDate = new Date(filters.maxReleaseDate);
 
-    const genres = filters.genres.map((e) => ({
-      genres: { $elemMatch: { id: e } },
+    const genres = filters.genres.map((genreId) => ({
+      genres: { $elemMatch: { id: genreId } },
     }));
 
-    const requiredGenres = filters.requiredGenres.map((e) => ({
-      genres: { $elemMatch: { id: e } },
+    const requiredGenres = filters.requiredGenres.map((genreId) => ({
+      genres: { $elemMatch: { id: genreId } },
     }));
 
-    return this.movieModel.find(
+    const movies = await this.movieModel.find(
       {
         $and: [
           {
@@ -49,5 +53,15 @@ export class MovieService {
       {},
       { sort: SORT_QUERY[filters.orderBy] },
     );
+
+    const movieIds = movies.map((movie) => movie._id);
+
+    const userVotes = await this.movieVoteModel.find({
+      userId: userId,
+      movieId: { $in: movieIds },
+    });
+
+    console.log(userVotes);
+    return movies;
   }
 }
