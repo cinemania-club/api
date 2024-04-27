a7076c063821008425580a94fdd6645fef7a7d82
Guto <gutomarzagao@gmail.com>
2024-04-27 18:32:16 -0300

    Delete playlist

--- src/playlist/playlist.controller.ts
+++ src/playlist/playlist.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post, Req } from "@nestjs/common";
+import { Body, Controller, Delete, Param, Post, Req } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { Request } from "express";
 import { Model } from "mongoose";
 import { Anonymous } from "src/auth/auth.guard";
 import { CreatePlaylistDto } from "./create-playlist.dto";
+import { DeletePlaylistDto } from "./delete-playlist.dto";
 import { Playlist } from "./playlist.schema";
 
 @Controller("/playlists")
@@ -14,15 +15,21 @@ export class PlaylistController {
 
   @Anonymous()
   @Post()
-  async createPlaylist(
-    @Req() req: Request,
-    @Body() playlist: CreatePlaylistDto,
-  ) {
+  async createPlaylist(@Req() req: Request, @Body() dto: CreatePlaylistDto) {
     const created = await this.playlistModel.create({
       userId: req.payload!.userId,
-      name: playlist.name,
+      name: dto.name,
     });
 
     return { id: created._id };
+  }
+
+  @Anonymous()
+  @Delete("/:id")
+  async deletePlaylist(@Req() req: Request, @Param() dto: DeletePlaylistDto) {
+    await this.playlistModel.findOneAndDelete({
+      _id: dto.id,
+      userId: req.payload!.userId,
+    });
   }
 }
