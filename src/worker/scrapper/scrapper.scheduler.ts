68978aae196e1d0f907adfdc7de2b503d387c6f3
Guto <gutomarzagao@gmail.com>
2024-05-01 19:41:07 -0300

    Schedule task to get popular movies and series everyday at midnight

--- src/worker/scrapper/scrapper.scheduler.ts
+++ src/worker/scrapper/scrapper.scheduler.ts
@@ -2,17 +2,19 @@ import { InjectQueue } from "@nestjs/bull";
 import { Injectable } from "@nestjs/common";
 import { Cron } from "@nestjs/schedule";
 import { Queue } from "bull";
-import { startOfDay, subDays } from "date-fns";
 import { BRT, isProduction } from "src/constants";
 
 @Injectable()
 export class ScrapperScheduler {
   constructor(@InjectQueue("tmdb") private tmdbQueue: Queue) {}
 
-  @Cron("0 0 12 * * *", { timeZone: BRT, disabled: !isProduction })
-  async getChanges() {
-    const today = startOfDay(new Date());
-    const yesterday = subDays(today, 1);
-    await this.tmdbQueue.add("getChanges", { date: yesterday, page: 1 });
+  @Cron("0 0 0 * * *", { timeZone: BRT, disabled: !isProduction })
+  async getPopularMovies() {
+    await this.tmdbQueue.add("getPopularMovies", { page: 1 });
+  }
+
+  @Cron("0 0 0 * * *", { timeZone: BRT, disabled: !isProduction })
+  async getPopularSeries() {
+    await this.tmdbQueue.add("getPopularSeries", { page: 1 });
   }
 }
