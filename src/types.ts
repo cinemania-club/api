1591603a09913dda9099667499bccf05a2bf0022
Guto <gutomarzagao@gmail.com>
2024-06-09 22:49:12 -0300

    Bugfix: move types.d.ts to types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,17 @@
+import { Oid } from "./mongo";
+
 export enum DataSource {
   INTERNAL = "INTERNAL",
   MOVIELENS = "MOVIELENS",
+}
+
+export type RequestPayload = { userId: Oid };
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Express {
+    interface Request {
+      payload?: RequestPayload;
+    }
+  }
 }
